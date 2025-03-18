0981b681b409e20fb47c8a6b19bb76158b7defb4
Bayan <bayan616m@gmail.com>
2025-03-18 14:40:51 +0300

    Refactor: editing on setting

--- src/components/SearchInput.tsx
+++ src/components/SearchInput.tsx
@@ -1,7 +1,8 @@
 import { Input } from "@chakra-ui/react";
 import { InputGroup } from "./ui/input-group";
-import { BsSearch } from "react-icons/bs";
+
 import { useRef } from "react";
+import { BsSearch } from "react-icons/bs";
 
 interface Props {
   onSearch: (searchText: string) => void;
